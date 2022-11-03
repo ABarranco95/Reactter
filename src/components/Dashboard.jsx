f544690af0e51982f8497e0d582ab6ba920f38d2
Angel <abarranco650@gmail.com>
2022-11-03 11:29:23 -0700

    imported logout functionality from firebase file logic

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,9 @@
-import { signOut } from "firebase/auth";
 import React , {useState, useEffect} from "react";
-import { auth } from "../firebase";
-import { Link, useNavigate, useParams } from "react-router-dom";
-
 import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logOut } from "../firebase";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { query, collection, getDocs, where } from "firebase/firestore";
+
 const Dashboard = () => {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
@@ -19,7 +19,7 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
         <button
           className="font-bold text-xl bg-red-500 rounded-lg p-1"
-          onClick={() => signOut(auth)}
+          onClick={() => logOut(auth)}
         >
           Sign Out
         </button>
